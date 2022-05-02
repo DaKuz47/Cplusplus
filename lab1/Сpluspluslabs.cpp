f3da16950b73ae2c945047c9ed48bceaa0ef18c4
DaKuz47 <73737160+DaKuz47@users.noreply.github.com>
2022-05-02 21:24:20 +0300

    Update Сpluspluslabs.cpp

--- lab1/Сpluspluslabs.cpp
+++ lab1/Сpluspluslabs.cpp
@@ -5,59 +5,51 @@ using namespace std;
 double diskremenant(double a, double b, double c) {
 	return b * b - 4 * a * c;
 }
-
+enum roots_count {
+	INF = -1,
+	ZERO,
+	ONE,
+	TWO
+};
 //roots calculationes
-vector<double> quad_eq_roots(double a, double b, double c) {
+vector<double> quad_eq_roots(vector<double> coef) {
+	double a = coef[0];
+	double b = coef[1];
+	double c = coef[2];
 	vector<double> roots{0};
-	if ((a != 0 && b != 0) || (a != 0 && c != 0)) {			//if roots[0] = 0 - no roots
-		double D = diskremenant(a, b, c);					//1 - 1 or 2 roots
-		if (D < 0) {										//2 - infinity roots
-			roots[0] = 0;
+	//Если пара a не 0 и ещё что-то не ноль, то считаем, как обычно
+	if ((a != 0 && b != 0) || (a != 0 && c != 0)) {			
+		double D = diskremenant(a, b, c);			
+		if (D < 0) {						
+			roots[0] = ZERO;
 		}
 		else {
-			roots[0] = 1;
+			roots[0] = ONE;
 			roots.push_back((-1 * b + sqrt(D)) / (2 * a));
 			if (D != 0) {
 				roots.push_back((-1 * b - sqrt(D)) / (2 * a));
+				roots[0] = TWO;
 			}
 		}
 	}
+	//Если только a ноль, то получаем линейное уравнение
 	else if (a == 0 && b != 0 && c != 0) {
-		roots[0] = 1;
+		roots[0] = ONE;
 		roots.push_back(-1 * c / b);
 	}
+	//Если все нули
 	else if (a + b + c == 0) {
-		roots[0] = 2;
+		roots[0] = INF;
 	}
+	// Если только c не ноль
 	else if (c != 0) {
-		roots[0] = 0;
+		roots[0] = ZERO;
 	}
+	//Если не ноль только a или только b
 	else {
-		roots[0] = 1;
+		roots[0] = ONe;
 		roots.push_back(0);
 	}
 	return roots;
-}
-
-int main()
-{
-	double a = 0, b = 0, c = 0;
-	cout << "Enter the coefficient of the quadtratic equation" << endl;
-	cin >> a >> b >> c;
-	vector<double> roots = quad_eq_roots(a, b, c);
-	if (roots[0] == 0) {
-		cout << "no routs" << endl;
-	}
-	else if (roots[0] == 2) {
-		cout << "roots is any number" << endl;
-	}
-	else {
-		cout << "roots: ";
-		roots.erase(roots.begin());
-		for (auto x : roots) {
-			cout << x << " ";
-		}
-		cout << endl;
-	}
 }
 
